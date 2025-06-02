3a8e9cde7a1297808b88fe332af1802ea6b0e70b
Abdullah12-tech <Abdullahakinkunmi26@gmail.com>
2025-06-02 14:01:36 +0100

    modified

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,18 +1,18 @@
-import React from 'react'
+// import React from 'react'
 
-const errorHandler = (err, req, res, next) => {
-    if(res.ok){
-        return res.status(200).json({
-            status: "success",
-            message: err.message
-        })
-    }
-    res.status(400).json({
-        status: "error",
-        message: "something went wrong"
-    })
-    next()
-}
+// const errorHandler = (err, req, res, next) => {
+//     if(res.ok){
+//         return res.status(200).json({
+//             status: "success",
+//             message: err.message
+//         })
+//     }
+//     res.status(400).json({
+//         status: "error",
+//         message: "something went wrong"
+//     })
+//     next()
+// }
 
 
-module.exports = errorHandler
+// module.exports = errorHandler
