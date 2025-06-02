4a10a815b3eb5d296229ade4c71b15e98d5e09c2
Abdullah12-tech <Abdullahakinkunmi26@gmail.com>
2025-06-02 14:05:52 +0100

    modified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("hello")
 const express = require("express")
-const { authRouter } = require("./router/authRouter")
+const authRouter = require("./router/authRouter")
+
 // const errorHandler = require("./middlewares/errorHandler")
 const app = express()
 app.listen(4006, ()=>{
