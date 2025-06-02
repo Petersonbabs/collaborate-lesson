78ee4c774742d36b46f8d5633aa58619500882fa
Abdullah12-tech <Abdullahakinkunmi26@gmail.com>
2025-06-02 16:56:04 +0100

    dev

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("hello")
 const express = require("express")
 const authRouter = require("./router/authRouter")
 
-// const errorHandler = require("./middlewares/errorHandler")
+const errorHandler = require("./middlewares/errorHandler")
 const app = express()
 app.listen(4006, ()=>{
     console.log("app is running on 4006")
@@ -12,4 +12,4 @@ app.listen(4006, ()=>{
 app.use(express.json())
 
 app.use("/api/auth", authRouter)
-// app.use(errorHandler)
+app.use(errorHandler)
