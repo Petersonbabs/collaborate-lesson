eba35a0d8a905f567539c70dbb42837e39c67583
ojoyetunde@gmail.com <ojoyetunde432@gmail.com>
2025-06-03 09:57:02 +0100

    transporter ready

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 console.log("hello")
 const express = require("express")
-// const  authRouter  = require("./router/authRouter")
 const errorHandler = require("./middlewares/errorHandler")
+require("./services/nodemailer/transporter")
 const authRouter = require("./router/authRouter")
 
 
 const app = express()
 app.listen(4006, ()=>{
     console.log("app is running on 4006")
-})
+})   
 
 
 app.use(express.json())
